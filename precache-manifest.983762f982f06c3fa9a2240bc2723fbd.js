bf7948ad6bbc3bb0f4bca45e91ffe3a190d423d8
ibrahim cisse <ckader065@gmail.com>
2019-09-24 19:52:28 -0400

    Updates

--- precache-manifest.983762f982f06c3fa9a2240bc2723fbd.js
+++ precache-manifest.983762f982f06c3fa9a2240bc2723fbd.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a9c4a31c6f78480913e36f300156599e",
+    "revision": "cd0275050537ab74a03b77a69fd39f6a",
     "url": "/progress-bar/index.html"
   },
   {
-    "revision": "c59cbd8ade0d42e833ba",
-    "url": "/progress-bar/static/css/main.83e80459.chunk.css"
+    "revision": "c052179eaa709cbc28d3",
+    "url": "/progress-bar/static/css/main.450cad74.chunk.css"
   },
   {
     "revision": "c1c576b6ed96eab9ce11",
     "url": "/progress-bar/static/js/2.516f9c3e.chunk.js"
   },
   {
-    "revision": "c59cbd8ade0d42e833ba",
+    "revision": "c052179eaa709cbc28d3",
     "url": "/progress-bar/static/js/main.14681819.chunk.js"
   },
   {
